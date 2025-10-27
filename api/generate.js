61e99ea34cbd63e613de0c17ee8a9d2787f095a3
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-10-27 23:43:13 +0500

    3

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
